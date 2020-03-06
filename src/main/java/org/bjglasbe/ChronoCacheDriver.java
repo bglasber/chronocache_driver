17f83e647cc6339fef829894cb22e4c056daeb93
Brad Glasbergen <bglasber@gmail.com>
2020-03-06 17:20:34 -0500

    Add Connection and Driver starting points.

---
+++ src/main/java/org/bjglasbe/ChronoCacheDriver.java
@@ -0,0 +1,55 @@
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverPropertyInfo;
+import java.util.logging.Logger;
+import java.util.Properties;
+
+public class ChronoCacheDriver implements Driver {
+
+    public ChronoCacheDriver() {
+    }
+    
+    // TODO: Static DriverManager register goes here.
+
+    @Override
+    public boolean acceptsURL( String url ) {
+        if( !url.startsWith( "jdbc:cc/" ) ) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public Connection connect( String url, Properties info ) {
+        return null;
+    }
+
+    @Override
+    public int getMajorVersion() {
+        return 1;
+    }
+
+    @Override
+    public int getMinorVersion() {
+        return 0;
+    }
+
+    @Override
+    public Logger getParentLogger() {
+        return Logger.getLogger( ChronoCacheDriver.class.getName() );
+    }
+
+    @Override
+    public DriverPropertyInfo[] getPropertyInfo(
+            String url,
+            Properties info
+    ) {
+        return null;
+    }
+
+    @Override
+    public boolean jdbcCompliant() {
+        return false;
+    }
+
+}
